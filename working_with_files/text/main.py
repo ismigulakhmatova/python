8ab460e3e431ca985ba2f4ffbe738ec2463cb7a5
ismigulakhmatova <ismigulakhmatova@gmail.com>
2025-04-29 19:36:39 +0500

    My python codes

---
+++ working_with_files/text/main.py
@@ -0,0 +1,13 @@
+# with open("input.txt", "r") as f:
+#     print(f.read())
+
+
+# with open("input.txt", "x") as f:
+#     f.write("Hello World")
+    
+# with open("input.txt", "w") as f:
+#     f.write("Hello World")
+
+# with open("input.txt", "a") as f:
+#     f.write("\nNew line")
+
